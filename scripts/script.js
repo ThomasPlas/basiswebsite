3fdcf5fddc75fa8d1966ecfa56013232fe55275b
ThomasPlas <145397194+ThomasPlas@users.noreply.github.com>
2024-11-21 20:47:22 +0100

    aanpassingen

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
 // JavaScript Document
 console.log("hi");
+
+
+let menu = document.querySelector("header > button");
+let navigatie = document.querySelector("nav");
+let menuSluiten = document.querySelector("nav > button");
+
+menu.addEventListener("click", function() {
+  console.log("open");
+  navigatie.classList.toggle("open");
+});
+
+menuSluiten.addEventListener("click", function() {
+  console.log("sluit");
+  navigatie.classList.remove("open");
+});
