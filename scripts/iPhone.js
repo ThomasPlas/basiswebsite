0f484c31e8c019af7e6ce53706e6ec8be74c933c
ThomasPlas <145397194+ThomasPlas@users.noreply.github.com>
2024-12-04 18:21:32 +0100

    1

---
+++ scripts/iPhone.js
@@ -0,0 +1,27 @@
+const iPhoneKleuren = document.querySelectorAll('input[name="iPhoneKleuren"]');
+const welkeTelefoon = document.querySelector('main section:nth-of-type(1) p:nth-of-type(2)');
+
+iPhoneKleuren.forEach(radio => {
+    radio.addEventListener('change', () => {
+
+        const value = radio.value;
+        
+        if (value === "alleKleuren") {
+            welkeTelefoon.textContent = "6,3-inch iPhone 16 Pro in vier kleuren";
+        }
+        else if (value === "zwartTitanium") {
+            welkeTelefoon.textContent = "6,9-inch iPhone 16 Pro Max1 en 6,3‑inch iPhone 16 Pro1 in zwart titanium";
+        }
+        else if (value === "witTitanium") {
+            welkeTelefoon.textContent = "6,9-inch iPhone 16 Pro Max1 en 6,3‑inch iPhone 16 Pro1 in wit titanium";
+        }
+        else if (value === "naturelTitanium") {
+            welkeTelefoon.textContent = "6,9-inch iPhone 16 Pro Max1 en 6,3‑inch iPhone 16 Pro1 in naturel titanium";
+        } 
+        else if (value === "desertTitanium") {
+            welkeTelefoon.textContent = "6,9-inch iPhone 16 Pro Max1 en 6,3‑inch iPhone 16 Pro1 in desert titanium";
+        }
+    });
+});
+
+// code even nakijken
